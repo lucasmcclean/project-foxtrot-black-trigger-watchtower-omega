f106176432ee3e10d58360069bdcf2dceda03c04
Arch <rhygonfn@gmail.com>
2025-10-25 20:12:56 -0400

    Player 2 assets

--- project-foxtrot-black-trigger-watchtower-omega/health_bar_eeg.gd
+++ project-foxtrot-black-trigger-watchtower-omega/health_bar_eeg.gd
@@ -3,7 +3,8 @@ extends ProgressBar
 @export var player: Player;
 
 func _ready() -> void:
-	max_value = 20
+    max_value = 20
 
 func _process(delta: float) -> void:
-	value = player.health
+    var tween: Tween = get_tree().create_tween()
+    tween.tween_property(self, "value", player.health, 0.2)
