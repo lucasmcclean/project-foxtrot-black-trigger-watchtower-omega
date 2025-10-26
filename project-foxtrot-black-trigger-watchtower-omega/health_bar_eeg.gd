6c3372b7bddc318548f9c2d78df4e0dde993274c
Arch <rhygonfn@gmail.com>
2025-10-25 20:49:32 -0400

    Particles

--- project-foxtrot-black-trigger-watchtower-omega/health_bar_eeg.gd
+++ project-foxtrot-black-trigger-watchtower-omega/health_bar_eeg.gd
@@ -1,10 +1,21 @@
 extends ProgressBar
 
-@export var player: Player;
+@export var player: Player
+@onready var particles: GPUParticles2D = $GlowParticle
+
+var last_health: int
 
 func _ready() -> void:
-    max_value = 20
+	max_value = 20
+	value = player.health
+	last_health = value
 
 func _process(delta: float) -> void:
-    var tween: Tween = get_tree().create_tween()
-    tween.tween_property(self, "value", player.health, 0.2)
+	if player.health < last_health:
+		particles.restart()
+		particles.emitting = true
+
+	var tween: Tween = get_tree().create_tween()
+	tween.tween_property(self, "value", player.health, 0.2)
+
+	last_health = player.health
